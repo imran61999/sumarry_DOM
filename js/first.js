b4e3021220908eea0613766d6a776137db5c73a8
imran61999 <smemrannit@gmail.com>
2024-02-12 13:00:38 +0600

    aded list

--- js/first.js
+++ js/first.js
@@ -1,1 +1,14 @@
 console.log("hello worlld");
+// all li select
+const list = document.getElementsByTagName("li");
+console.log(list);
+
+const alltitle = document.getElementsByClassName("section-title");
+console.log(alltitle);
+
+const sectionTitle = document.getElementById("first-section");
+console.log(sectionTitle);
+
+sectionTitle.style.color="violet";
+sectionTitle.style.backgroundColor= "aqua";
+
---
+++ js/second.js
@@ -0,0 +1,7 @@
+
+const firstlist=document.getElementById("first-list");
+
+const list =document.createElement("li");
+list.innerText = "hello list";
+
+firstlist.appendChild(list);
